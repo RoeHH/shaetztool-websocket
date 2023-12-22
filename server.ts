0b7915fa3fdcd1a0cfa90239e6843f50ac07dc4d
roehh <git@roeh.ch>
2023-12-22 15:48:40 +0100

    Server Ready

--- server.ts
+++ server.ts
@@ -1,62 +1,58 @@
 import { serve } from "https://deno.land/std@0.166.0/http/server.ts";
 import { Server } from "https://deno.land/x/socket_io@0.2.0/mod.ts";
-import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
- 
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts"; 
  
 const app = new Application();
- 
+
 interface User {
   id: string,
   name: string,
   vote: number | undefined
 };
  
-let intervalFinished = false;
 const kv = await Deno.openKv();
 const router = new Router();
- 
-let users: unknown[];
- 
+  
 router
   .get("/vote/:team", async (context) => {
-    if (!intervalFinished) {
+    const intervalFinished = await kv.get(["intervalFinished", context.params.team]);   
+    if (!intervalFinished.value) {
       const users = [];
       const iter = await kv.list({ prefix: ["user", context.params.team]});
       for await (const res of iter) users.push(res.value);
+      context.response.headers.set("Access-Control-Allow-Origin", "*");
       context.response.body = JSON.stringify(users);
     }
   })
   .post("/vote/:team/:name/:vote", async (context) => {
-    if (intervalFinished) {
+    const intervalFinished = await kv.get(["intervalFinished", context.params.team]);    
+    if (intervalFinished.value) {
       const { team, name, vote } = context.params;
       const user: User = {
         id: crypto.randomUUID(),
         name: name,
         vote: Number(vote)
       };
- 
-      const result = await kv.set(["user", team, name], user);
+      await kv.set(["user", team, name], user);
+      context.response.headers.set("Access-Control-Allow-Origin", "*");
       context.response.body = "voted";
     }
   })
   .post("/vote/:team/start", async (context) => {
     const iter = await kv.list({ prefix: ["user", context.params.team]});
     for await (const res of iter) kv.delete(res.key);
-
-    console.log(context.params.team, "hello");
     const team = context.params.team;
-    intervalFinished = true;
+    await kv.set(["intervalFinished", team], true);
     let count = 5;
-    const timer = setInterval(function() {
-      console.log(count);
+    const timer = setInterval(async function() {
       io.local.emit(team, count)
       if (count === 0) {
         clearInterval(timer);
-        intervalFinished = false;
+        await kv.set(["intervalFinished", team], false);
       }
       count--;
     }, 1000);
+    context.response.headers.set("Access-Control-Allow-Origin", "*");
     context.response.body = "started";
   });
  
@@ -65,10 +61,9 @@ const io = new Server({cors: {origin: "*"}});
 const handler = io.handler(async (req) => {
   return await app.handle(req) || new Response(null, { status: 403 });
 });
- 
+
 app.use(router.routes());
-app.use(oakCors({ origin: "*" }));
- 
+
 await serve(handler, {
   port: 3001,
 });
